c5a8af72a16cf0256f2b4c6e3310f9548f86f568
SerhiiShapovalov <ShapovalovSV83@gmail.com>
2023-07-05 14:06:59 +0300

    Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,10 +1,19 @@
-import { MdDownloading } from 'react-icons/md';
+import { MagnifyingGlass } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 function Loader() {
   return (
     <div className={css.wrapper}>
-      <MdDownloading className={css.loader} />
+      <MagnifyingGlass
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="MagnifyingGlass-loading"
+        wrapperStyle={{}}
+        wrapperClass="MagnifyingGlass-wrapper"
+        glassColor="#c0efff"
+        color="#ffee00"
+      />
     </div>
   );
 }
